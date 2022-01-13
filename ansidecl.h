83a380ae072faa0b861d54f5d274e9f2659fb62d
Garry T. Williams <gtwilliams@gmail.com>
2022-01-13 13:40:39 -0500

    Correct the FSF address.

--- ansidecl.h
+++ ansidecl.h
@@ -13,7 +13,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with the GNU C Library; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301 USA  */
 
 /* ANSI and traditional C compatibility macros
 
