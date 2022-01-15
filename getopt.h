12d86140c10a0f8ec11a7f81cc5d21c1bf2ddcf9
Garry T. Williams <gtwilliams@gmail.com>
2022-01-14 22:03:30 -0500

    Correct FSF address.

--- getopt.h
+++ getopt.h
@@ -15,7 +15,7 @@
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, write to the Free
    Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-   MA 02110-1301 USA 02111-1307 USA.  */
+   MA 02110-1301 USA.  */
 
 #ifndef _GETOPT_H
 
